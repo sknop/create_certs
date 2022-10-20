1ad3897fe47017857afb3da7367395e51a8f0a50
Sven Erik Knop <seknop@gmail.com>
2022-10-20 17:37:48 +0000

    Now with key and imported into KJS keystore

--- create_certs.sh
+++ create_certs.sh
@@ -16,6 +16,8 @@ CAfile=$1
 CAKeyfile=$2
 hostfile=$3
 
+password="changeme"
+
 function create_cert_for_host() {
     host=$1
     echo "Creating cert for $host"
@@ -26,6 +28,13 @@ function create_cert_for_host() {
 
     echo "Will be signed by $CAfile with $CAKeyfile"
     openssl x509 -req -CA $CAfile -CAkey $CAKeyfile  -in certs/$host.csr -out certs/$host.crt -days 3650 -CAcreateserial
+
+    echo "Convert to pkc12 file"
+    openssl pkcs12 -password pass:$password -export -in certs/$host.crt -inkey certs/$host.key -out certs/$host.p12 -name service
+
+    keytool -importkeystore \
+        -deststorepass $password -destkeypass $password  -destkeystore certs/$host-keystore.jks \
+        -srckeystore certs/$host.p12 -srcstoretype PKCS12 -srcstorepass $password -alias service
 }
 
 # ensure certs directory is available
